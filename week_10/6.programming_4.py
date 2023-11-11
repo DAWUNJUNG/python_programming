3d5537b6ed7e26551611e2bd11820c63b44227aa
dawun <dawun@lacy.co.kr>
2023-11-11 15:55:27 +0900

    10주차 과제 수정

--- week_10/6.programming_4.py
+++ week_10/6.programming_4.py
@@ -1,7 +1,9 @@
 ori_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+print('실행전', ori_list)
+
 for i in range(len(ori_list)):
     if 3 <= i <= 8:
         ori_list[i] = int('-' + str(ori_list[i]))
 
-print(ori_list)
+print('실행후', ori_list)
