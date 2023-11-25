242cc33a85feaac0092785966860aae94479808e
dawun <dawun@lacy.co.kr>
2023-11-25 15:12:17 +0900

    12주차

--- week_13/23.student_score_process.py
+++ week_13/23.student_score_process.py
@@ -10,7 +10,13 @@ def main():
             name, number = get_contact()
             address_book.pop(name)
         elif user == 3:
-            pass  # 도전 문제 참조
+            name = input('검색할 사람의 이름을 입력해주세요.')
+            if name in address_book.keys():
+                showInfo = input(f"{name}으로 등록된 연락처가 있습니다.\n정보를 보여드릴까요? (Y:예 / N:아니요)")
+                if showInfo == 'Y':
+                    print(f"{name}의 전화번호 : {address_book[name]}")
+            else:
+                print(f"{name}으로 등록된 연락처가 없습니다.")
         elif user == 4:
             for key in sorted(address_book):
                 print(key, '의 전화번호:', address_book[key])
