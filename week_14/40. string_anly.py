b38ebcae2f52a1c499b8fd33c2eed8bc4b7b5e02
dawun <dawun@lacy.co.kr>
2023-12-02 14:45:59 +0900

    14주차

--- week_14/40. string_anly.py
+++ week_14/40. string_anly.py
@@ -1,14 +1,16 @@
 # 문자열 분석
+import re
+
 sentence = input("문자열을 입력하시오: ")
 
 table = {"alphas": 0, "digits": 0, "spaces": 0}
 
 for i in sentence:
-    if i.isalpha():
+    if re.compile('[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]').match(i):
         table["alphas"] += 1
-    if i.isdigit():
+    if re.compile('[0-9]').match(i):
         table["digits"] += 1
-    if i.isspace():
+    if re.compile('\s').match(i):
         table["spaces"] += 1
 
 print(table)
